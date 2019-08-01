89eaaf837e1526faf3629e49c99daf891e9d5949
Enrico Bianchi <enrico.bianchi@gmail.com>
2019-08-01 17:18:33 +0200

    Added flags

--- main.go
+++ main.go
@@ -4,14 +4,26 @@ import "gopkg.in/alecthomas/kingpin.v2"
 
 const VERSION = "0.0.1"
 
-func dump() {
+func dump(host *string, port *int, index *string) {
 
 }
 
 func main() {
+	host := kingpin.Flag("host", "Set the Elasticsearch host (default localhost)").
+		Short('H').
+		Default("localhost").
+		String()
+	port := kingpin.Flag("port", "Set the Elasticsearch port (default 9200)").
+		Short('P').
+		Default("9200").
+		Int()
+	index := kingpin.Arg("index", "Index to dump").
+		Required().
+		String()
 
+	kingpin.HelpFlag.Short('h')
 	kingpin.Version(VERSION)
 	kingpin.Parse()
 
-	dump()
+	dump(host, port, index)
 }
